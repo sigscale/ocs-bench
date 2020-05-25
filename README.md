ed6037ef664194240012ea0cb629b148d19c13f4
Vance Shipley <vances@sigscale.org>
2020-05-25 13:45:36 +0800

    genesis

---
+++ README.md
@@ -0,0 +1,4 @@
+# [SigScale](http://www.sigscale.org) OCS Benchmark Testing
+
+This project is used to simulate subscriber traffic for SigScale OCS.
+
---
+++ src/ocs_bench.erl
@@ -0,0 +1,34 @@
+%%% ocs_bench.erl
+%%% vim: ts=3
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @copyright 2020 SigScale Global Inc.
+%%% @end
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc This library module implements the public API for the
+%%%   {@link //ocs_bench. ocs_bench} application.
+%%%
+-module(ocs_bench).
+-copyright('Copyright (c) 2020 SigScale Global Inc.').
+
+%% export the ocs_bench  public API
+-export([]).
+
+%%----------------------------------------------------------------------
+%%  The ocs_bench public API
+%%----------------------------------------------------------------------
+
+%%----------------------------------------------------------------------
+%%  internal functions
+%%----------------------------------------------------------------------
+
