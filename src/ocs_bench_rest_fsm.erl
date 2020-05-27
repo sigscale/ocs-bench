8d413c7c1c0ea19f4114ba0098a54379159cf4d3
Vance Shipley <vances@sigscale.org>
2020-05-27 08:10:31 +0800

    rest transactions adding client and services

--- src/ocs_bench_rest_fsm.erl
+++ src/ocs_bench_rest_fsm.erl
@@ -31,15 +31,23 @@
 -export([init/1, handle_event/4, callback_mode/0,
 			terminate/3, code_change/4]).
 %% export the callbacks for gen_statem states.
--export([transaction/3]).
+-export([client_request/3, client_response/3, wait_request/3, wait_response/3]).
 
--type state() :: transaction.
+-include_lib("kernel/include/inet.hrl").
+
+-type state() :: client_request | client_response
+		| wait_request | wait_response.
 
 -record(statedata,
 		{active :: pos_integer(),
 		mean :: pos_integer(),
 		deviation :: 0..100,
-		service :: term()}).
+		start :: pos_integer(),
+		uri :: string(),
+		auth :: {Athorization :: string(), BasicAuth :: string()},
+		hostname :: undefined | string(),
+		address :: undefined | inet:ip_address(),
+		request :: reference()}).
 -type statedata() :: #statedata{}.
 
 %%----------------------------------------------------------------------
@@ -79,21 +87,188 @@ init(_Args) ->
 	{ok, Mean} = application:get_env(mean),
 	{ok, Deviation} = application:get_env(deviation),
 	{ok, Uri} = application:get_env(rest_uri),
-	{ok, transaction, #statedata{active = Active, mean = Mean,
-			deviation = Deviation, service = Uri}, rand:uniform(4000)}.
+	{ok, User} = application:get_env(rest_user),
+	{ok, Password} = application:get_env(rest_pass),
+	UserPass = User ++ ":" ++ Password,
+	BasicAuth = base64:encode_to_string(UserPass),
+	Authorization = {"authorization", "Basic " ++ BasicAuth},
+	service = ets:new(service, [public, named_table]),
+	Data = #statedata{active = Active, mean = Mean,
+			deviation = Deviation, uri = Uri,
+			auth = Authorization},
+	{ok, client_request, Data, rand:uniform(4000)}.
 
--spec transaction(EventType, EventContent, Data) -> Result
+-spec client_request(EventType, EventContent, Data) -> Result
 	when
 		EventType :: gen_statem:event_type(),
 		EventContent :: term(),
 		Data :: statedata(),
 		Result :: gen_statem:event_handler_result(state()).
-%% @doc Handles events received in the <em>transaction</em> state.
+%% @doc Handles events received in the <em>wait_request</em> state.
 %% @private
 %%
-transaction(timeout = _EventType, _EventContent, #statedata{} = Data) ->
+client_request(timeout = _EventType, _EventContent,
+		#statedata{address = undefined, hostname = undefined,
+		uri = Uri, auth = Authorization} = Data) ->
 	Start = erlang:system_time(millisecond),
-	{next_state, transaction, Data, timeout(Start, Data)}.
+	{ok, Hostname} = inet:gethostname(),
+	case inet:gethostbyname(Hostname, inet) of
+		{ok, #hostent{h_addrtype = inet, h_addr_list = [Address | _]}} ->
+			Path = "/ocs/v1/client/" ++ inet:ntoa(Address),
+			Accept = {"accept", "application/json"},
+			Request = {Uri ++ Path, [Authorization, Accept]},
+			case httpc:request(get, Request, [], [{sync, false}], tmf) of
+				{ok, RequestId} when is_reference(RequestId) ->
+					NewData = Data#statedata{request = RequestId,
+							start = Start, address = Address,
+							hostname = Hostname},
+					{next_state, client_response, NewData};
+				{error, Reason} ->
+					{stop, Reason, Data}
+			end;
+		{error, Reason} ->
+			{stop, Reason, Data}
+	end;
+client_request(timeout = _EventType, _EventContent,
+		#statedata{address = Address, hostname = Hostname,
+		uri = Uri, auth = Authorization} = Data)
+		when is_tuple(Address), is_list(Hostname) ->
+	Start = erlang:system_time(millisecond),
+	Id = inet:ntoa(Address),
+	Path = Uri ++ "/ocs/v1/client",
+	ContentType = "application/json",
+	Accept = {"accept", ContentType},
+	RequestHeaders = [Authorization, Accept],
+	RequestBody = zj:encode(#{"id" => Id, "protocol" => "DIAMETER"}),
+	Request = {Path, RequestHeaders, ContentType, RequestBody},
+	case httpc:request(post, Request, [], [{sync, false}], tmf) of
+		{ok, RequestId} when is_reference(RequestId) ->
+			NewData = Data#statedata{request = RequestId, start = Start},
+			{next_state, client_response, NewData};
+		{error, Reason} ->
+			{stop, Reason, Data}
+	end;
+client_request(_EventType, _EventContent, _Data) ->
+keep_state_and_data.
+
+-spec client_response(EventType, EventContent, Data) -> Result
+	when
+		EventType :: gen_statem:event_type(),
+		EventContent :: term(),
+		Data :: statedata(),
+		Result :: gen_statem:event_handler_result(state()).
+%% @doc Handles events received in the <em>client_response</em> state.
+%% @private
+%%
+client_response(info = _EventType,
+		{http, {RequestId, {{_, 404, _}, _Headers, _Body}}} = _EventContent,
+		#statedata{request = RequestId, start = Start} = Data) ->
+		NewData = Data#statedata{request = undefined},
+	{next_state, client_request, NewData, timeout(Start, Data)};
+client_response(info = _EventType,
+		{http, {RequestId, {{_, StatusCode, _}, _Headers, Body}}},
+		#statedata{request = RequestId, start = Start} = Data)
+		when StatusCode == 200; StatusCode == 201 ->
+	case zj:decode(Body) of
+		{ok, #{"id" := Id}} ->
+			case inet:parse_address(Id) of
+				{ok, Address} ->
+					NewData = Data#statedata{address = Address},
+					{next_state, wait_request, NewData, timeout(Start, NewData)};
+				{error, Reason} ->
+					{stop, Reason, Data}
+			end;
+		{Error, _Partial, _Remaining} when Error == error; Error == incomplete ->
+			{stop, Error, Data}
+	end;
+client_response(info = _EventType,
+		{http, {RequestId, {{_, StatusCode, _}, _Headers, _Body}}},
+		#statedata{request = RequestId} = Data) ->
+	{stop, StatusCode, Data};
+client_response(info = _EventType, {http, {RequestId, {error, Reason}}},
+		#statedata{request = RequestId} = Data) ->
+	{stop, Reason, Data}.
+
+-spec wait_request(EventType, EventContent, Data) -> Result
+	when
+		EventType :: gen_statem:event_type(),
+		EventContent :: term(),
+		Data :: statedata(),
+		Result :: gen_statem:event_handler_result(state()).
+%% @doc Handles events received in the <em>wait_request</em> state.
+%% @private
+%%
+wait_request(timeout = _EventType, _EventContent,
+		#statedata{active = Active, uri = Uri, auth = Authorization} = Data) ->
+	Start = erlang:system_time(millisecond),
+	case ets:info(service, size) of
+		N when N < Active ->
+			Path = Uri ++ "/serviceInventoryManagement/v2/service/",
+			ContentType = "application/json",
+			Accept = {"accept", ContentType},
+			RequestHeaders = [Authorization, Accept],
+			Chars = [#{"name" => "serviceIdentity", "value" => imsi()},
+					#{"name" => "serviceAkaK",
+						"value" => binary_to_hex(crypto:strong_rand_bytes(16))},
+					#{"name" => "serviceAkaOPc",
+						"value" => binary_to_hex(crypto:strong_rand_bytes(16))}],
+			ServiceSpecification = #{"id" => "1",
+					"href" => "/catalogManagement/v2/serviceSpecification/1"},
+			RequestBody = zj:encode(#{"serviceCharacteristic" => Chars,
+					"serviceSpecification" => ServiceSpecification}),
+			Request = {Path, RequestHeaders, ContentType, RequestBody},
+			case httpc:request(post, Request, [], [{sync, false}], tmf) of
+				{ok, RequestId} when is_reference(RequestId) ->
+					NewData = Data#statedata{request = RequestId, start = Start},
+					{next_state, wait_response, NewData};
+				{error, Reason} ->
+					{stop, Reason, Data}
+			end;
+		_N ->
+			keep_state_and_data
+	end.
+
+-spec wait_response(EventType, EventContent, Data) -> Result
+	when
+		EventType :: gen_statem:event_type(),
+		EventContent :: term(),
+		Data :: statedata(),
+		Result :: gen_statem:event_handler_result(state()).
+%% @doc Handles events received in the <em>wait_response</em> state.
+%% @private
+%%
+wait_response(info = _EventType,
+		{http, {RequestId, {{_, 201, _}, _Headers, Body}}} = _EventContent,
+		#statedata{request = RequestId, start = Start} = Data) ->
+		NewData = Data#statedata{request = undefined},
+	case zj:decode(Body) of
+		{ok, #{"serviceCharacteristic" := Chars}} ->
+			F = fun(#{"name" := "serviceIdentity", "value" := Id}, {_, K, OPc}) ->
+						{Id, K, OPc};
+					(#{"name" := "serviceAkak", "value" := K}, {Id, _, OPc}) ->
+						{Id, K, OPc};
+					(#{"name" := "serviceOPc", "value" := OPc}, {Id, K, _}) ->
+						{Id, K, OPc};
+					(_, Acc) ->
+						Acc
+			end,
+			case lists:foldl(F, {undefined, undefined, undefined}, Chars) of
+				{Id, K, OPc} when is_list(Id) ->
+					ets:insert(service, {Id, K, OPc}),
+					{next_state, wait_request, NewData, timeout(Start, Data)};
+				_ ->
+					{stop, bad_service, NewData}
+			end;
+		{Error, _Partial, _Remaining} when Error == error; Error == incomplete ->
+			{stop, Error, NewData}
+	end;
+wait_response(info = _EventType,
+		{http, {RequestId, {{_, StatusCode, _}, _Headers, _Body}}},
+		#statedata{request = RequestId} = Data) ->
+	{stop, StatusCode, Data};
+wait_response(info = _EventType, {http, {RequestId, {error, Reason}}},
+		#statedata{request = RequestId} = Data) ->
+	{stop, Reason, Data}.
 
 -spec handle_event(EventType, EventContent, State, Data) -> Result
 	when
@@ -105,8 +280,8 @@ transaction(timeout = _EventType, _EventContent, #statedata{} = Data) ->
 %% @doc Handles events received in any state.
 %% @private
 %%
-handle_event(_EventType, _EventContent, State, Data) ->
-	{next_state, State, Data}.
+handle_event(_EventType, _EventContent, _State, _Data) ->
+	keep_state_and_data.
 
 -spec terminate(Reason, State, Data) -> any()
 	when
@@ -164,4 +339,28 @@ timeout(Start, #statedata{mean = Mean, deviation = Deviation}) ->
 		_Interval ->
 			0
 	end.
+
+-spec imsi() -> [$0..$9].
+%% @doc Generate an IMSI.
+%% @private
+imsi() ->
+	Charset = lists:seq($0, $9),
+	imsi(Charset, []).
+%% @hidden
+imsi(Charset, Acc) when length(Acc) < 9 ->
+	N = lists:nth(rand:uniform(10), Charset),
+	imsi(Charset, [N | Acc]);
+imsi(_, Acc) ->
+	"001001" ++ Acc.
+
+%% @hidden
+binary_to_hex(B) when is_binary(B) ->
+	binary_to_hex(B, []).
+%% @hidden
+binary_to_hex(<<N:4, Rest/bits>>, Acc) when N >= 10 ->
+	binary_to_hex(Rest, [N - 10 + $a | Acc]);
+binary_to_hex(<<N:4, Rest/bits>>, Acc) ->
+	binary_to_hex(Rest, [N + $0 | Acc]);
+binary_to_hex(<<>>, Acc) ->
+	lists:reverse(Acc).
 
